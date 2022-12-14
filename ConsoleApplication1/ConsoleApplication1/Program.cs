b1ff4614f9f1a6a0b9dd1421d597b3fb7b2d101c
bobrynja <119498240+bobrynja@users.noreply.github.com>
2022-12-14 15:14:07 +0300

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите строку: ");
+                string a = Console.ReadLine();
+                Console.Write("Введите кол-во часов: ");
+                int n = Convert.ToInt32(Console.ReadLine());
+                string reg = @"\d{2}:\d{2}";
+                Regex regex = new Regex(reg);
+                MatchCollection matches = regex.Matches(a);
+                for (int i = 0; i < matches.Count; i++)
+                {
+                    string[] hhss = matches[i].Value.Split(':');
+                    int hh = Convert.ToInt32(hhss[0]);
+                    int mm = Convert.ToInt32(hhss[1]);
+                    if (hh >= 0 && hh < 24 && mm >= 0 && mm < 60)
+                    {
+                        if (n > 24) n %= 24;
+                        var time = new TimeSpan(hh, mm, 0).Add(new TimeSpan(-n, 0, 0));
+                        string timer = Convert.ToString(time) + ":" + mm.ToString();
+                        a = Regex.Replace(a, matches[i].Value, time.ToString(@"hh\:mm"));
+                    }
+                }
+
+                Console.WriteLine(a);
+            }
+            catch { Console.WriteLine("Некорректный ввод данных!!!"); }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string a = textBox1.Text;
+                int n = Convert.ToInt32(textBox3.Text);
+                string reg = @"\d{2}:\d{2}";
+                Regex regex = new Regex(reg);
+                MatchCollection matches = regex.Matches(a);
+                for (int i = 0; i < matches.Count; i++)
+                {
+                    string[] hhss = matches[i].Value.Split(':');
+                    int hh = Convert.ToInt32(hhss[0]);
+                    int mm = Convert.ToInt32(hhss[1]);
+                    if (hh >= 0 && hh < 24 && mm >= 0 && mm < 60)
+                    {
+                        if (n > 24) n %= 24;
+                        var time = new TimeSpan(hh, mm, 0).Add(new TimeSpan(-n, 0, 0));
+                        string timer = Convert.ToString(time) + ":" + mm.ToString();
+                        a = Regex.Replace(a, matches[i].Value, time.ToString(@"hh\:mm"));
+                    }
+                }
+
+                textBox2.Text = a;
+                //foreach (Match item in matches) { Console.WriteLine(item.ToString()); }
+            }
+            catch { MessageBox.Show("Некорректный ввод данных!!!"); }
+        }
+    }
+}
